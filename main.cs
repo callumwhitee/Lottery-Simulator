82ac25cd2bbb7526ad5c93846fec0a29707ba15d
Callum White <62078259+cwite@users.noreply.github.com>
2021-11-10 10:23:51 +0000

    userInput

--- main.cs
+++ main.cs
@@ -3,14 +3,13 @@ using System;
 class Program {
 
   static int[] randomNumbers = new int[6];
+  static int[] userNumbers = new int[6];
   static int max = 200;
+  static int length = randomNumbers.Length;
 
 
   public static void Main (string[] args) {
-    for(int i = 0; i < randomNumbers.Length; i++)
-    {
-      Console.WriteLine(randomNumbers[i]);
-    }
+    GenerateNumbers();
   }
 
 
@@ -18,10 +17,30 @@ class Program {
 
   static void GenerateNumbers()
   {
-    for(int i = 0; i < randomNumbers.Length; i++)
+    for(int i = 0; i < length; i++)
     {
       Random random = new Random();
       randomNumbers[i] = random.Next(0, max);
+    }
+  }
+
+
+
+  static void userInput()
+  {
+    Console.WriteLine("Please Enter 6 Numbers From 1 to 200");
+    for(int i = 0; i < userNumbers.Length; i++)
+    {
+      Console.Write("> ");
+      userNumbers[i] = Console.ReadLine();
+    }
+  }
+
+  static void Match()
+  {
+    for(int i = 0; i < userNumbers.Length; i++)
+    {
+      
     }
   }
 }
