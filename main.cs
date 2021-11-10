f8298ee05849223cb0ac8bccd98cfab683252b5e
Callum White <62078259+cwite@users.noreply.github.com>
2021-11-10 10:13:32 +0000

    small code cahnges

--- main.cs
+++ main.cs
@@ -1,7 +1,27 @@
 using System;
 
 class Program {
+
+  static int[] randomNumbers = new int[6];
+  static int max = 200;
+
+
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+    for(int i = 0; i < randomNumbers.Length; i++)
+    {
+      Console.WriteLine(randomNumbers[i]);
+    }
+  }
+
+
+
+
+  static void GenerateNumbers()
+  {
+    for(int i = 0; i < randomNumbers.Length; i++)
+    {
+      Random random = new Random();
+      randomNumbers[i] = random.Next(0, max);
+    }
   }
 }
